5b5a19387c942e3e8068cd3459a1f1ba769f51c0
=Cristobal M. Amaya <=cristobal.molinaamaya@georgebrown.ca>
2020-10-06 22:14:06 -0400

    Adding a react excerises lab 5

---
+++ build/precache-manifest.334198b1e0fe575d54f6b2c198c3adb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3f9ad0ec9173b94a8a2",
+    "url": "/static/css/main.30590161.chunk.css"
+  },
+  {
+    "revision": "a3f9ad0ec9173b94a8a2",
+    "url": "/static/js/main.a3f9ad0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "4b9cca836c26836291dbe85b03267f67",
+    "url": "/index.html"
+  }
+];
